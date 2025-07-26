5cbafd5c2ed0ebb7c97f3affa0767eb4f2a52f34
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2025-07-26 01:11:31 -0300

    Criando o mapeamento JPA para a entidade produto

--- src/test/java/br/com/leonardogtc/produtosapi/model/Produto.java
+++ src/test/java/br/com/leonardogtc/produtosapi/model/Produto.java
@@ -1,11 +1,33 @@
 package br.com.leonardogtc.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 // Nota: POJO (Plain Old Java Object) é uma classe simples que não depende de nenhum framework específico.
+
+/**
+ * Classe que representa um produto.
+ * Esta classe é um POJO (Plain Old Java Object) que será mapeado para uma tabela no banco de dados usando JPA (Java Persistence API).
+ * A annotation @Entity indica que esta classe é uma entidade JPA e será persistida no banco de dados.
+ * Os atributos desta classe representam as colunas da tabela correspondente.
+ */
+@Entity
+@Table(name = "produtos") // A annotation @Table é usada para mapear a classe para uma tabela específica no banco de dados.
 public class Produto {
 
+    @Column(name = "id") // A annotation @Column é usada para mapear o atributo id para uma coluna no banco de dados.
+    @Id // A annotation @Id indica que este atributo é a chave primária da entidade.
     private String id;
+
+    @Column
     private String nome;
+
+    @Column
     private String descricao;
+
+    @Column
     private Double preco;
 
     public String getId() {
